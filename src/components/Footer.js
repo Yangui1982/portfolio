38a04c826c113a0bde5682e565683456d92f7c00
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2024-03-10 01:36:32 +0100

    custom footer done

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2024 Portfolio</p>
+    </footer>
+  );
+};
+
+export default Footer;
